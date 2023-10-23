685b58d2664ceeda2000a3f9e8ef6ef61b69e18d
codegrann <omondihills02@gmail.com>
2023-10-23 22:32:21 +0300

    .

--- to-do-listapp/src/components/Task.js
+++ to-do-listapp/src/components/Task.js
@@ -1,11 +1,22 @@
-function Task({ taskTitle }) {
+import { useState } from "react";
+
+function Task({ taskTitle, days }) {
+  const [done, setDone] = useState(false);
+
+  function setTaskDone() {
+    setDone(true);
+  }
+
   return (
+  {done ? null : (
     <div className="task" style={{ display: "flexbox", gap: "2rem" }}>
       <input type="checkbox" />
       <input type="text" name="" value={taskTitle} />
-      <button>X</button>
-      <p style={{ display: "inline" }}>3 days to go</p>
+      <button onClick={setTaskDone}>X</button>
+      <p style={{ display: "inline" }}>{days} days to go</p>
     </div>
-  );
+  )}
+)
+
 }
 export default Task;
--- to-do-listapp/src/components/TaskList.js
+++ to-do-listapp/src/components/TaskList.js
@@ -1,11 +1,13 @@
+import { useState } from "react";
 import Task from "./Task";
 function TaskList({ tasks }) {
   const taskTitle = "Finish my project";
+  const days = 10;
   // const deadLine = prompt("dd/mm/yy");
   return (
     <div>
       <p>This tasklist includes: </p>
-      <Task taskTitle={taskTitle} />
+      <Task taskTitle={taskTitle} days={days} />
     </div>
   );
 }
