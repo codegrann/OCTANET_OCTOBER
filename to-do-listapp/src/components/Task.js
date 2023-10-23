2f0f31a29ab368441708d321e676679e2e27c1e3
codegrann <omondihills02@gmail.com>
2023-10-23 22:41:52 +0300

    .

--- to-do-listapp/src/components/Task.js
+++ to-do-listapp/src/components/Task.js
@@ -2,21 +2,27 @@ import { useState } from "react";
 
 function Task({ taskTitle, days }) {
   const [done, setDone] = useState(false);
+  const [checked, setChecked] = useState(false);
 
   function setTaskDone() {
     setDone(true);
   }
+  function checking() {
+    setChecked(true);
+  }
 
-  return (
-  {done ? null : (
+  return done ? null : (
     <div className="task" style={{ display: "flexbox", gap: "2rem" }}>
-      <input type="checkbox" />
-      <input type="text" name="" value={taskTitle} />
+      <input type="checkbox" onClick={checking} />
+      <input
+        type="text"
+        name=""
+        value={taskTitle}
+        style={{ textDecoration: checked ? "line-through" : "none" }}
+      />
       <button onClick={setTaskDone}>X</button>
       <p style={{ display: "inline" }}>{days} days to go</p>
     </div>
-  )}
-)
-
+  );
 }
 export default Task;
