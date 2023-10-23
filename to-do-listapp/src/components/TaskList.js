ff4de78fc4fd46d3617ae4508d40e8085e6f09b0
codegrann <omondihills02@gmail.com>
2023-10-23 23:04:55 +0300

    .

--- to-do-listapp/src/components/TaskList.js
+++ to-do-listapp/src/components/TaskList.js
@@ -3,8 +3,19 @@ import Task from "./Task";
 function TaskList({ tasks }) {
   const [taskList, setTaskList] = useState([
     {
+      id: 1,
       taskTitle: "Finish my project",
       days: 10,
+    },
+    {
+      id: 2,
+      taskTitle: "Visit the deans office",
+      days: 3,
+    },
+    {
+      id: 3,
+      taskTitle: "Write a thesis",
+      days: 15,
     },
   ]);
 
@@ -18,9 +29,9 @@ function TaskList({ tasks }) {
         <input type="text" placeholder="enter title of your task" />
         <button>Add</button>
       </div>
-      {taskList.map((task) => {
-        <Task taskTitle={task.taskTitle} days={task.days} />;
-      })}
+      {taskList.map((task) => (
+        <Task id={task.id} taskTitle={task.taskTitle} days={task.days} />
+      ))}
       {/* <Task /> */}
     </div>
   );
