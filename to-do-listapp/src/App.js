558041cb485f74b8309ccfa448612e1c30e5f5f7
codegrann <omondihills02@gmail.com>
2023-10-30 20:26:19 +0300

    .

--- to-do-listapp/src/App.js
+++ to-do-listapp/src/App.js
@@ -1,25 +1,11 @@
-import { useState } from "react";
-import Task from "./components/Task";
+// import { useState } from "react";
+// import Task from "./components/Task";
 import TaskList from "./components/TaskList";
 
 function App() {
-  const [tasks, setTasks] = useState([
-    {
-      a: 2,
-    },
-    {
-      a: 3,
-    },
-    {
-      a: 4,
-    },
-    {
-      a: 5,
-    },
-  ]);
   return (
     <div>
-      <TaskList tasks={tasks} />
+      <TaskList />
     </div>
   );
 }
