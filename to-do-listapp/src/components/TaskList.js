69cd34924b88a8b339a118dc5b6b2d05de24b295
codegrann <omondihills02@gmail.com>
2023-11-01 06:19:05 +0300

    .

--- to-do-listapp/src/components/TaskList.js
+++ to-do-listapp/src/components/TaskList.js
@@ -11,11 +11,16 @@ function TaskList({ tasks }) {
   const [title, setTitle] = useState("");
 
   function addTask(e) {
+    let dueDate = prompt("Due date: yyyy/mm/dd");
+    const days = new Date(dueDate);
+
+    // ADD TASK TO THE LIST OF TASKS
     setTaskList([
       ...taskList,
       {
         id: taskList.length - 1,
         taskTitle: title,
+        days: days,
       },
     ]);
   }
@@ -32,7 +37,6 @@ function TaskList({ tasks }) {
         />
         <button onClick={addTask}>Add</button>
       </div>
-
       {taskList.map((task) => (
         <Task id={task.id} taskTitle={task.taskTitle} days={task.days} />
       ))}
